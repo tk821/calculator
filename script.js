07f190cd51a4b7276e6607aa62c0b386ca84b5a9
tk821 <191364051+tk821@users.noreply.github.com>
2025-01-21 21:05:24 -0800

    added event listeners for all buttons
    added event listeners for each button and some
    placeholder functions that they call

--- script.js
+++ script.js
@@ -28,3 +28,47 @@ function operate(operator, num1, num2) {
             return 'Error';
     }
 }
+
+function newNum(num) {
+    console.log(num);
+}
+
+function newOp(op) {
+    console.log(op);
+}
+
+function updateDisplay(num) {
+    const displayWindow = document.getElementById('display-text');
+    displayWindow.textContent = num;
+}
+
+let numberBtns = [];
+for (let i = 0; i < 10; i++) {
+    numberBtns.push(document.getElementById(i));
+}
+const numBtns = numberBtns;
+numberBtns = null;
+for (let i = 0; i < 10; i++) { //adds event listeners for each number button
+    numBtns[i].addEventListener('click', () => newNum(i));
+}
+
+//adds event listeners for each individual operator
+const clrBtn = document.getElementById('clear');
+clrBtn.addEventListener('click', () => clear);
+const equalBtn = document.getElementById('=');
+equalBtn.addEventListener('click', () => newOp('='));
+const divBtn = document.getElementById('/');
+divBtn.addEventListener('click', () => newOp('/'));
+const multBtn = document.getElementById('x');
+multBtn.addEventListener('click', () => newOp('x'));
+const subBtn = document.getElementById('-');
+subBtn.addEventListener('click', () => newOp('-'));
+const addBtn = document.getElementById('+');
+addBtn.addEventListener('click', () => newOp('+'));
+
+
+
+
+let numberOne;
+let numberTwo;
+let operator;

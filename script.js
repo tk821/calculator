ca3d68ce912d0f92e87aa762e5d5ea762a451357
tk821 <191364051+tk821@users.noreply.github.com>
2025-01-22 15:23:00 -0800

    pressing buttons now calls right functions
    pressing buttons now calls the function they are
    meant to call, and each function does what it's
    supposed to do

--- script.js
+++ script.js
@@ -14,15 +14,16 @@ function divide(dividend, divisor) {
     return dividend / divisor;
 }
 
-function operate(operator, num1, num2) {
-    switch (operator) {
-        case 'add':
+function operate(op, num1, num2) {
+    operator = null;
+    switch (op) {
+        case '+':
             return add(num1, num2);
-        case 'subtract': 
+        case '-': 
             return subtract(num1, num2)
-        case 'multiply': 
+        case 'x': 
             return multiply(num1, num2);
-        case 'divide':
+        case '/':
             return divide(num1, num2);
         default:
             return 'Error';
@@ -31,15 +32,59 @@ function operate(operator, num1, num2) {
 
 function newNum(num) {
     console.log(num);
+    if (exists(operator)) {
+        if (exists(numberOne)) {
+            numberTwo = num;
+            updateDisplay(num);
+        }
+        else {
+            alert('Error: tried to assign num2 with no num1');
+        }
+    }
+    else {
+        numberOne = num;
+        updateDisplay(num);
+    }
 }
 
 function newOp(op) {
     console.log(op);
+    if(!exists(numberTwo)) {
+        operator = op;
+    }
+    else {
+        equals();
+        operator = op;
+    }
+}
+
+function equals() {
+    console.log('=');
+    if (exists(numberOne) && exists(numberTwo) && exists(operator)) {
+            let result = operate(operator, numberOne, numberTwo);
+            updateDisplay(result);
+            numberOne = result;
+            numberTwo = null;
+            console.log(result);       
+    }
+}
+
+function exists(item) {
+    return (item !== undefined && item !== null);
 }
 
 function updateDisplay(num) {
     const displayWindow = document.getElementById('display-text');
     displayWindow.textContent = num;
+    
+}
+
+function clear() {
+    console.log('Clear');
+    numberOne = null;
+    numberTwo = null;
+    operator = null;
+    updateDisplay(0);
 }
 
 let numberBtns = [];
@@ -54,9 +99,11 @@ for (let i = 0; i < 10; i++) { //adds event listeners for each number button
 
 //adds event listeners for each individual operator
 const clrBtn = document.getElementById('clear');
-clrBtn.addEventListener('click', () => clear);
+clrBtn.addEventListener('click', clear);
+
 const equalBtn = document.getElementById('=');
-equalBtn.addEventListener('click', () => newOp('='));
+equalBtn.addEventListener('click', equals);
+
 const divBtn = document.getElementById('/');
 divBtn.addEventListener('click', () => newOp('/'));
 const multBtn = document.getElementById('x');
@@ -65,8 +112,6 @@ const subBtn = document.getElementById('-');
 subBtn.addEventListener('click', () => newOp('-'));
 const addBtn = document.getElementById('+');
 addBtn.addEventListener('click', () => newOp('+'));
-
-
 
 
 let numberOne;
